88c43f0ced827e0926c2d33f46555e1b3d5d888e
Bagher Keshmiri <bagherkeshmiri@gmail.com>
2023-06-18 15:16:18 +0330

    remove useless codes

--- routes/api/api-v1.php
+++ routes/api/api-v1.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\api\v1\user\AuthController;
 use App\Http\Controllers\api\v1\user\LinkController;
 use App\Http\Controllers\api\v1\user\TokenController;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,18 +11,16 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::post('/register', [AuthController::class,'register']);
+Route::post('/register', [AuthController::class, 'register']);
 
 
-Route::middleware(['apiHeadersCheck','auth:sanctum'])->group(function(){
-
-    Route::prefix('link')->controller(LinkController::Class)->group( function(){
+Route::middleware(['apiHeadersCheck', 'auth:sanctum'])->group(function () {
+    Route::prefix('link')->controller(LinkController::Class)->group(function () {
         Route::get('/shorten', 'shorten');
         Route::get('/all', 'all');
     });
 
 
-    Route::post('/tokens/create', [TokenController::class , 'generateToken'])->withoutMiddleware(['apiHeadersCheck','auth:sanctum']);
-
+    Route::post('/tokens/create', [TokenController::class, 'generateToken'])->withoutMiddleware(['apiHeadersCheck', 'auth:sanctum']);
 });
 
